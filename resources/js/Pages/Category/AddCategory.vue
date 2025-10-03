5b92fdc602631fe4c661e98f6bb79e980f5e5bea
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2025-10-03 15:52:46 +0700

    change icon cart

--- resources/js/Pages/Category/AddCategory.vue
+++ resources/js/Pages/Category/AddCategory.vue
@@ -41,16 +41,16 @@ const cartItems = ref([]);
                         <div class="inline-flex items-center pr-4">
                             <Link :href="route('cart')" aria-label="Keranjang"
                                 class="relative z-10 px-4 py-2 bg-blue-600 text-white rounded-l-lg hover:bg-blue-700 transition flex items-center justify-center shadow-md">
-                                <!-- Badge angka item -->
-                                <span
-                                    class="absolute -top-2 -right-1 bg-green-500 text-white text-xs font-bold rounded-full w-6 h-6 flex items-center justify-center border-2 border-white">
-                                    {{ cartItems.length }}
-                                </span>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                                    stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M3 3h2l.4 2M7 13h10l3-7H5.4M7 13L5.4 5M7 13l-2 9m12-9l2 9M9 21a2 2 0 11-4 0 2 2 0zm10 0a2 2 0 11-4 0 2 2 0" />
-                                </svg>
+                            <!-- Badge angka item -->
+                            <span
+                                class="absolute -top-2 -right-1 bg-green-500 text-white text-xs font-bold rounded-full w-6 h-6 flex items-center justify-center border-2 border-white">
+                                {{ cartItems.length }}
+                            </span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                                stroke="currentColor" class="h-5 w-5">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                            </svg>
                             </Link>
                             <div class="px-4 py-2 bg-blue-50 text-blue-700 rounded-r-lg text-sm font-semibold">
                                 Total Tagihan {{ totalTagihan }}
