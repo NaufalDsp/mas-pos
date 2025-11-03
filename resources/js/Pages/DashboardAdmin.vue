c7f74b70dfedb234bcadc9d37752c4124e20b97c
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2025-11-03 20:17:02 +0700

    fix: error dashboard admin

--- resources/js/Pages/DashboardAdmin.vue
+++ resources/js/Pages/DashboardAdmin.vue
@@ -279,6 +279,8 @@ onMounted(() => {
         
 
         
+
+        
         <!-- Stats Cards -->
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-6">
             <!-- Total Users -->
